fe473998fd8f0cf516644cf623da084074252548
s-g-holden <sholden@worth.systems>
2022-01-14 11:42:22 +0000

    fork orig

--- omm-be/routes/student.route.js
+++ omm-be/routes/student.route.js
@@ -1,9 +1,9 @@
 let mongoose = require('mongoose'),
   express = require('express'),
-  router = express.Router();
+  router = express.Router()
 
 // Student Model
-let studentSchema = require('../models/Student');
+let studentSchema = require('../models/Student')
 
 // CREATE Student
 router.route('/create-student').post((req, res, next) => {
@@ -15,7 +15,7 @@ router.route('/create-student').post((req, res, next) => {
       res.json(data)
     }
   })
-});
+})
 
 // READ Students
 router.route('/').get((req, res) => {
@@ -39,33 +39,36 @@ router.route('/edit-student/:id').get((req, res) => {
   })
 })
 
-
 // Update Student
 router.route('/update-student/:id').put((req, res, next) => {
-  studentSchema.findByIdAndUpdate(req.params.id, {
-    $set: req.body
-  }, (error, data) => {
-    if (error) {
-      return next(error);
-      console.log(error)
-    } else {
-      res.json(data)
-      console.log('Student updated successfully !')
-    }
-  })
+  studentSchema.findByIdAndUpdate(
+    req.params.id,
+    {
+      $set: req.body,
+    },
+    (error, data) => {
+      if (error) {
+        return next(error)
+        console.log(error)
+      } else {
+        res.json(data)
+        console.log('Student updated successfully !')
+      }
+    },
+  )
 })
 
 // Delete Student
 router.route('/delete-student/:id').delete((req, res, next) => {
   studentSchema.findByIdAndRemove(req.params.id, (error, data) => {
     if (error) {
-      return next(error);
+      return next(error)
     } else {
       res.status(200).json({
-        msg: data
+        msg: data,
       })
     }
   })
 })
 
-module.exports = router;
+module.exports = router
